6df5f65d1535652bb30ca38c763536cd9e97f772
Jimmy <jimmy@r3e.network>
2026-03-14 19:29:22 +0700

    Remove stale debugging and migration scripts

--- apps/web/test-neon.js
+++
@@ -1,6 +0,0 @@
-import { sc } from "@cityofzion/neon-js";
-
-const payloadMsg = { type: "String", value: "Hello World" };
-const param = sc.ContractParam.fromJson(payloadMsg);
-
-console.log(param);
